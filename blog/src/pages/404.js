55a8f8acd87e6d5eda65a095cf4c4e90b7d5d203
luizfverissimo <62751844+luizfverissimo@users.noreply.github.com>
2020-11-29 12:28:27 -0300

    cleanup start gatsby

---
+++ blog/src/pages/404.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>404</h1>
+    <p>
+      Ops não tem nada aqui! <span role="img" aria-label='Chorando'>😭</span>
+    </p>
+  </Layout>
+)
+
+export default NotFoundPage
