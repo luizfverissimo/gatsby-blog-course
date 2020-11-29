af67b718804771693ac24e8b39db3ae360b80cd6
luizfverissimo <62751844+luizfverissimo@users.noreply.github.com>
2020-11-29 14:28:26 -0300

    course start

---
+++ blog/src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Profile() {
+  return (
+    <div className="Profile-wrapper">
+      <h1>LF Verissimo</h1>
+      <h2>Programador Fullstack</h2>
+      <p>Programador frontend na Agência Mandarin</p>
+    </div>
+  )
+}
+
+export default Profile
